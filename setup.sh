ad682f43f1405482bff8e2e6b298be1ce8074504
ikuto32 <47182163+ikuto32@users.noreply.github.com>
2025-07-14 09:07:39 +0900

    Make CUDA optional

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -e
 python3 -m pip install --user pybind11
 
 # Create build directory and build the Python extension
+export CMAKE_PREFIX_PATH="$(python3 -m pybind11 --cmakedir):${CMAKE_PREFIX_PATH}"
 mkdir -p build
 cd build
 cmake -DUSE_CUDA=OFF ..   # set to ON when the CUDA toolkit is available
